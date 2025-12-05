9a84940ffd17885688f721cba0a499792bd672dd
David de Fitero Teijeira <dde-fite@student.42madrid.com>
2025-12-05 20:34:30 +0100

    function for initializing the stacks

--- src/main.c
+++ src/main.c
@@ -6,25 +6,46 @@
 /*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 20:37:15 by dde-fite          #+#    #+#             */
-/*   Updated: 2025/12/04 18:57:24 by dde-fite         ###   ########.fr       */
+/*   Updated: 2025/12/05 20:21:23 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*initialize_stacks(int argc, char *argv[])
+int	*initialize_stacks(t_stack **lst, char *argv[])
 {
-	t_stack	*lst;
+	t_stack	*_lst;
+	char	*_tmp;
 
-	lst = mal
-	while ()
+	while (*argv)
 	{
-		
+		_tmp = **argv;
+		while (*_tmp)
+		{
+			if (ft_isdigit(_tmp))
+			{
+				_lst = lstnew(ft_atoi(_tmp));
+				if (!_lst)
+					return (memory_error(lst));
+				lstadd_back(lst, _lst);
+			}
+			else
+			{
+				if (!ft_isspace)
+					return (memory_error(lst));
+				_tmp++;
+			}
+		}
 	}
 }
 
 int	main(int argc, char *argv[])
 {
+	t_stack	*lst;
+
+	lst = NULL;
 	if (argc < 2)
 		return (parsing_error());
+	if (initialize_stacks(&lst, argv))
+		return (-1);
 }
