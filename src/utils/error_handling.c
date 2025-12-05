7090f3c77360a310d339921869e053e963d445b2
David de Fitero Teijeira <dde-fite@student.42madrid.com>
2025-12-05 20:32:13 +0100

    error handler for cleaning memory

--- src/utils/error_handling.c
+++ src/utils/error_handling.c
@@ -6,7 +6,7 @@
 /*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 15:53:27 by dde-fite          #+#    #+#             */
-/*   Updated: 2025/12/04 18:08:01 by dde-fite         ###   ########.fr       */
+/*   Updated: 2025/12/05 20:22:36 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 int	parsing_error(void)
 {
+	ft_putchar(ERROR_MESSAGE, 2);
+	return (-1);
+}
+
+int	memory_error(t_stack *lst)
+{
+	lstclear(&lst);
 	ft_putchar(ERROR_MESSAGE, 2);
 	return (-1);
 }
