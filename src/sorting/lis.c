49855214bd4a48e8f205500368efd78219f80e7d
dde-fite <nora@defitero.com>
2025-12-14 22:59:47 +0100

    minor changes for improving lis

--- src/sorting/lis.c
+++ src/sorting/lis.c
@@ -6,61 +6,62 @@
 /*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 17:27:05 by dde-fite          #+#    #+#             */
-/*   Updated: 2025/12/12 21:29:08 by dde-fite         ###   ########.fr       */
+/*   Updated: 2025/12/13 23:10:06 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static t_stack	*get_lis(t_global *global_stack, t_stack *stack)
+static t_stack	*get_lis(t_stack *stack, bool *error)
 {
-	int const	stack_size = lstsize(stack);
-	int			*lis_count;
+	int const	stack_size = ft_maxnbr(0, lstsize(stack) - 1);
+	int			*lis;
 	int			i;
 	int			acc;
 	t_stack		*_stack;
 
-	i = 0;
-	lis_count = ft_calloc(stack_size, sizeof(int));
-	if (!lis_count)
+	lis = ft_calloc(stack_size + 1, sizeof(int));
+	if (!lis)
 	{
-		global_stack->error = true;
+		*error = true;
 		return (NULL);
 	}
+	i = 0;
 	while (i < stack_size)
 	{
 		_stack = get_by_index(stack, i);
+		acc = INT_MIN;
 		while (_stack)
 		{
-			acc = INT_MIN;
 			if (_stack->number > acc)
 			{
 				acc = _stack->number;
-				lis_count[i]++;
+				lis[i]++;
 			}
 			_stack = _stack->next;
 		}
 		i++;
 	}
 	return (get_by_index(stack,
-			(int)(lis_count - get_max_arr_value(lis_count))));
+			(int)(lis - get_max_arr_value(lis))));
 }
 
 void	push_nolis_to_b(t_global *global_stacks)
 {
-	t_stack const	*lis = get_lis(global_stacks->stack_a);
+	t_stack const	*lis = get_lis(global_stacks->stack_a,
+			&global_stacks->error);
 	t_stack			*_stack_a;
 	int				acc;
 
 	if (!lis)
 		return ;
 	_stack_a = global_stacks->stack_a;
-	acc = INT_MIN;
 	while (_stack_a && _stack_a != lis)
 	{
 		pb(global_stacks);
 		_stack_a = _stack_a->next;
 	}
+	acc = INT_MIN;
 	while (_stack_a)
 	{
 		if (_stack_a->number > acc)
