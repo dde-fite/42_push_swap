5d3492fcff6f78fb83ea65d1777ada957125f2d3
dde-fite <nora@defitero.com>
2025-12-23 00:38:12 +0100

    deleted letter put in by mistake in test.fish

--- test.fish
+++ test.fish
@@ -1,46 +1,91 @@
 #!/usr/bin/fish
-echo "============= PUSH_SWAP_TESTER =============";
-echo "                  dde-fite                  ";
-echo "";
+echo "============= PUSH_SWAP_TESTER ============="
 
-echo "Testing 100 Numbers";
-echo " - Test 1:";
-set args (seq -214747 214747 | sort -R | head -n 100); ./push_swap $args | wc -l;
-./push_swap $args | ./checker $args;
+echo "                  dde-fite                  "
+
 echo ""
-echo " - Test 2:";
-set args (seq -214747 214747 | sort -R | head -n 100); ./push_swap $args | wc -l;
-./push_swap $args | ./checker $args;
+
+echo "Testing 100 Numbers"
+
+echo " - Test 1:"
+
+set args (seq -214747 214747 | sort -R | head -n 100)
+./push_swap $args | wc -l
+
+./push_swap $args | ./checker $args
+
 echo ""
-echo " - Test 3:";
-set args (seq -214747 214747 | sort -R | head -n 100); ./push_swap $args | wc -l;
-./push_swap $args | ./checker $args;
+echo " - Test 2:"
+
+set args (seq -214747 214747 | sort -R | head -n 100)
+./push_swap $args | wc -l
+
+./push_swap $args | ./checker $args
+
 echo ""
-echo " - Test 4:";
-set args (seq -214747 214747 | sort -R | head -n 100); ./push_swap $args | wc -l;
-./push_swap $args | ./checker $args;
+echo " - Test 3:"
+
+set args (seq -214747 214747 | sort -R | head -n 100)
+./push_swap $args | wc -l
+
+./push_swap $args | ./checker $args
+
 echo ""
-echo " - Test 5:";
-set args (seq -214747 214747 | sort -R | head -n 100); ./push_swap $args | wc -l;
-./push_swap $args | ./checker $args;
-echo "";
-echo "Testing 500 Numbers";
-echo " - Test 1:";
-set args (seq -214747 214747 | sort -R | head -n 500); ./push_swap $args | wc -l;
-./push_swap $args | ./checker $args;
+echo " - Test 4:"
+
+set args (seq -214747 214747 | sort -R | head -n 100)
+./push_swap $args | wc -l
+
+./push_swap $args | ./checker $args
+
 echo ""
-echo " - Test 2:";
-set args (seq -214747 214747 | sort -R | head -n 500); ./push_swap $args | wc -l;
-./push_swap $args | ./checker $args;
+echo " - Test 5:"
+
+set args (seq -214747 214747 | sort -R | head -n 100)
+./push_swap $args | wc -l
+
+./push_swap $args | ./checker $args
+
 echo ""
-echo " - Test 3:";
-set args (seq -214747 214747 | sort -R | head -n 500); ./push_swap $args | wc -l;
-./push_swap $args | ./checker $args;
+
+echo "Testing 500 Numbers"
+
+echo " - Test 1:"
+
+set args (seq -214747 214747 | sort -R | head -n 500)
+./push_swap $args | wc -l
+
+./push_swap $args | ./checker $args
+
 echo ""
-echo " - Test 4:";
-set args (seq -214747 214747 | sort -R | head -n 500); ./push_swap $args | wc -l;
-./push_swap $args | ./checker $args;
+echo " - Test 2:"
+
+set args (seq -214747 214747 | sort -R | head -n 500)
+./push_swap $args | wc -l
+
+./push_swap $args | ./checker $args
+
 echo ""
-echo " - Test 5:";
-set args (seq -214747 214747 | sort -R | head -n 500); ./push_swap $args | wc -l;
-./push_swap $args | ./checker $args;3
+echo " - Test 3:"
+
+set args (seq -214747 214747 | sort -R | head -n 500)
+./push_swap $args | wc -l
+
+./push_swap $args | ./checker $args
+
+echo ""
+echo " - Test 4:"
+
+set args (seq -214747 214747 | sort -R | head -n 500)
+./push_swap $args | wc -l
+
+./push_swap $args | ./checker $args
+
+echo ""
+echo " - Test 5:"
+
+set args (seq -214747 214747 | sort -R | head -n 500)
+./push_swap $args | wc -l
+
+./push_swap $args | ./checker $args
+
